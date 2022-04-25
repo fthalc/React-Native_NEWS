d7b73a74877b7e36ab3f4a43cb78d04d16214ceb
fthalc <fatih.alici44@gmail.com>
2022-04-25 17:31:37 +0300

    searchbar added

--- App.js
+++ App.js
@@ -1,54 +1,65 @@
-import React from 'react';
-import { Text, FlatList, SafeAreaView, ImageBackground, Dimensions,TouchableHighlight, Alert, Platform, StatusBar} from 'react-native';
+import React,{useState } from 'react';
+import { Text, FlatList,View ,TextInput, SafeAreaView, ImageBackground, Dimensions,TouchableHighlight, Alert, Platform, StatusBar} from 'react-native';
  
 const App = ({navigation}) => {
+  const [text, setText] = useState('');
   const numColumns = 3;
   const tileWidth = Dimensions.get('window').width / numColumns;
   const imageBaseUrl = "https://images.unsplash.com/photo-";
   const imageParameters = "?auto=format&fit=crop&w=375&q=80";
   const dataSource = [
-    { category: 'Technology', imageId: '1478358161113-b0e11994a36b' },
-    { category: 'Spor', imageId: '1521412644187-c49fa049e84d' },
-    { category: 'Health', imageId: '1526256262350-7da7584cf5eb' },
-    { category: 'Economy', imageId: '1542222024-c39e2281f121' },
-    { category: 'Education', imageId: '1503676260728-1c00da094a0b' },
-    { category: 'Music', imageId: '1511671782779-c97d3d27a1d4' },
-    { category: 'Theatre', imageId: '1507924538820-ede94a04019d' },
-    { category: 'Cinema', imageId: '1478720568477-152d9b164e26' },
-    { category: 'Weather', imageId: '1530908295418-a12e326966ba' },
-    { category: 'Trip', imageId: '1473625247510-8ceb1760943f' },
-    { category: 'Astrology', imageId: '1532968961962-8a0cb3a2d4f5' },
-    { category: 'Car', imageId: '1537041373298-55dbb337e651' },
-    { category: 'Gallery', imageId: '1500051638674-ff996a0ec29e' },
-    { category: 'Video', imageId: '1524253482453-3fed8d2fe12b' },
+    { category: 'Technology',name:'Teknoloji', imageId: '1478358161113-b0e11994a36b' },
+    { category: 'Sports',name:'Spor', imageId: '1521412644187-c49fa049e84d' },
+    { category: 'Science',name:'Bilim', imageId: '1532968961962-8a0cb3a2d4f5' },
+    { category: 'Health', name:'Sağlık',imageId: '1526256262350-7da7584cf5eb' },
+    { category: 'Business',name:'İş', imageId: '1542222024-c39e2281f121' },
+    { category: 'Entertainment',name:'Eğlence', imageId: '1503676260728-1c00da094a0b' },
+    { category: 'General',name:'Genel', imageId: '1494059980473-813e73ee784b' },
   ];
 
   const renderItem = ({ item }) => {
     return (
-     <TouchableHighlight onPress={() => navigation.navigate('Headlines', { category: item.category })}>
+     <TouchableHighlight onPress={() => navigation.navigate('Headlines', { category: item.category,name:item.name })}>
         <ImageBackground
         source={{ uri: imageBaseUrl + item.imageId + imageParameters }}
+        blurRadius={7}
+        resizeMode='cover'
         style={{
+          margin:1,
           width: tileWidth,
           height: tileWidth,
           justifyContent: 'center',
         }}>
-        <Text style={{
+        <Text style={{ 
+          fontWeight:'bold',
           textAlign: 'center',
           color: '#fff',
-          fontSize: 15
-        }}>{item.category}</Text>
+          fontSize: 25
+        }}>{item.name}</Text>
       </ImageBackground>
      </TouchableHighlight>
   );
   }
   return (
-    <FlatList
+    <SafeAreaView>
+       <View style={{padding: 10}}>
+      <TextInput
+        style={{height: 40}}
+        placeholder="Ara.."
+        onChangeText={newText => setText(newText)}
+        defaultValue={text}
+      />
+      
+      
+    </View>
+      <FlatList
         data={dataSource}
         renderItem={renderItem}
-        keyExtractor={(item) => item.category}
+        keyExtractor={(item) => item.name}
         numColumns={numColumns}
       />
+    </SafeAreaView>
+    
   );
 };
 App.navigationOptions = ({ navigation }) => ({
--- Headlines.js
+++ Headlines.js
@@ -4,20 +4,36 @@ import prettyTime from './PrettyTime';
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 const Headlines = ({navigation}) => {
-    const query = navigation.state.params && navigation.state.params.category;
+  console.log(navigation.state.params.category);
+    const category = navigation.state.params.category;
     const [headlines, setHeadlines] = useState({});
     const country = 'tr';
-    const API_KEY = '411ebf2636714e03a2bd211b253b2155';
-    const url = `https://newsapi.org/v2/top-headlines?
-    country=${country}&q=${query}&apiKey=${API_KEY}`;
-
+    const API_KEY2 = '411ebf2636714e03a2bd211b253b2155';
+    const API_KEY = '5a38b9e3f0034a14a8bcd4a3f6887b97';
+    const arrayCategory = ['Technology', 'Sports', 'Science',' Health',' Business',' Entertainment'];
+    let url;
+    let isIn = false;
+    arrayCategory.forEach(element => {
+      if(element === navigation.state.params.category){
+        isIn = true
+        return ;
+       }
+    });
+    url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${API_KEY}`;
     useEffect(() => {
       fetchData();
     }, []);
     async function fetchData(){
+      if(isIn){
+        
+      }else{
+        url = `https://newsapi.org/v2/top-headlines?country=${country}&q=${category}&apiKey=${API_KEY}`;
+        isIn = false;
+      }
         (await fetch(url))
         .json()
         .then(res => setHeadlines(res));
+        
     }
     function removeSource(title){
         if(title == null || title.indexOf('-') < 0) return title;
@@ -31,7 +47,7 @@ const Headlines = ({navigation}) => {
             <View style={{ flex: 1, flexDirection: 'row', padding: 10, borderBottom: 1, borderBottomWidth: 1, borderBottomColor: '#eee' }}>
                 <Image style={{ width: 100, height: 100 }} source={{ uri: item.urlToImage }} />
                 <View style={{ flex: 1, paddingLeft: 10 }}>
-                <Text style={{ flexWrap: 'wrap' }}>{removeSource(item.title)}</Text>
+                <Text style={{ flexWrap: 'wrap', fontWeight:'bold' }}>{removeSource(item.title)}</Text>
                 <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'flex-end' }}>
                     <View style={{ flexDirection: 'row', alignItems:'center' }}>
                         <Icon name="newspaper-variant-multiple-outline" size={15} style={{paddingRight: 5}}/>
@@ -47,7 +63,7 @@ const Headlines = ({navigation}) => {
         </TouchableHighlight>);
       }
     return (
-        <SafeAreaView>
+        <SafeAreaView style={{backgroundColor:'#e2e2e2'}}>
         <FlatList
           data={headlines.articles}
           renderItem={renderItem}
@@ -55,9 +71,11 @@ const Headlines = ({navigation}) => {
         />
       </SafeAreaView>
     );
+    
 };
+
 Headlines.navigationOptions = ({ navigation }) => ({
-    title: `${navigation.state.params && navigation.state.params.category} Haberleri`
+    title: `${navigation.state.params && navigation.state.params.name} Haberleri`
   });
    
   
